5f2971630cdbb52b59b2f17c6bfccf8e077fb39c
victor truong <2truong93@gmail.com>
2019-02-20 10:16:40 -0500

    update instructor to set the name attribute

--- character.py
+++ character.py
@@ -8,4 +8,8 @@
 #     pass
 # class used like def and is capitalized
 class Character():
-    pass
+    # "dunder init" method is constructor (double underscore init)
+    def __init__(self, new_name):
+#        #  `self` is the customary way to refer to instance being built
+#       #  in some other languages, they use `this`
+        self.name = new_name
