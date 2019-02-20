1a881f1d03866b2eda55cc8ac7c4cfc62f594159
victor truong <2truong93@gmail.com>
2019-02-20 11:48:27 -0500

    adds greet method and tests

--- character.py
+++ character.py
@@ -15,3 +15,6 @@ class Character():
         self.name = new_name
         self.avatar = new_avatar
         self.inventory = []
+
+    def greet(self):
+        return "Hello, I am %s. I am awesome. " % (self.name)
