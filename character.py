ef3d250e3a4e3aa54d1ac422aea01bfdd4a85b11
victor truong <2truong93@gmail.com>
2019-02-20 11:19:52 -0500

    adds avatar attribute and tests it

--- character.py
+++ character.py
@@ -9,7 +9,8 @@
 # class used like def and is capitalized
 class Character():
     # "dunder init" method is constructor (double underscore init)
-    def __init__(self, new_name):
+    def __init__(self, new_name, new_avatar):
 #        #  `self` is the customary way to refer to instance being built
 #       #  in some other languages, they use `this`
         self.name = new_name
+        self.avatar = new_avatar
---
+++ tests.py
@@ -0,0 +1,10 @@
+# these are not real tests
+from character import Character
+# characters can be instantiated with name and avatar
+
+arya = Character("Arya Stark","arya.png" )
+
+jon = Character("Jon Snow", "jon.png")
+
+print(arya.name, arya.avatar)
+print(jon.name, jon.avatar)
